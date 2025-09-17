b223d1bc45cd7dedea43fcf059637eb721094309
adilaslamkv <151339462+adilaslamkv@users.noreply.github.com>
2025-09-17 15:20:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Flutter Developer", "Frontend Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+var typed1 = new Typed(".text1", {
+    strings: ["Web Developer", "Flutter Developer", "Frontend Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
